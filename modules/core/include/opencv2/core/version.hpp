ef943e271dc33d685bc1902f61688ee7d9f974d4
Andrey Pavlenko <andrey.pavlenko@itseez.com>
2015-02-04 16:35:09 +0300

    fixing spaces for conformance

--- modules/core/include/opencv2/core/version.hpp
+++ modules/core/include/opencv2/core/version.hpp
@@ -10,7 +10,7 @@
 //                        Intel License Agreement
 //                For Open Source Computer Vision Library
 //
-// Copyright( C) 2000, Intel Corporation, all rights reserved.
+// Copyright (C) 2000, Intel Corporation, all rights reserved.
 // Copyright (C) 2011-2013, NVIDIA Corporation, all rights reserved.
 // Third party copyrights are property of their respective owners.
 //
@@ -32,10 +32,10 @@
 // warranties of merchantability and fitness for a particular purpose are disclaimed.
 // In no event shall the Intel Corporation or contributors be liable for any direct,
 // indirect, incidental, special, exemplary, or consequential damages
-//(including, but not limited to, procurement of substitute goods or services;
+// (including, but not limited to, procurement of substitute goods or services;
 // loss of use, data, or profits; or business interruption) however caused
 // and on any theory of liability, whether in contract, strict liability,
-// or tort(including negligence or otherwise) arising in any way out of
+// or tort (including negligence or otherwise) arising in any way out of
 // the use of this software, even if advised of the possibility of such damage.
 //
 //M*/
